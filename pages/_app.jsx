31adb39d2e9c2d81ba9de0d2548350d9518c0787
studio659 <studio@avlanche.fr>
2026-04-29 11:47:03 +0200

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,25 +1,10 @@
 import '../styles/globals.css'
-import Head from 'next/head'
+import { RateProvider } from '../lib/rateContext'
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#0d0d0d" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-        <meta name="apple-mobile-web-app-title" content="Avlanche" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/icon-32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/icon-16.png" />
-        <link rel="shortcut icon" href="/icons/favicon.ico" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152.png" />
-        <link rel="apple-touch-icon" sizes="192x192" href="/icons/icon-192.png" />
-        <title>Avlanche Music</title>
-      </Head>
+    <RateProvider>
       <Component {...pageProps} />
-    </>
+    </RateProvider>
   )
 }
